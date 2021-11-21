3837aa87832284a3cc665659d650c2e9b4723cd6
JigSaW <misho_1998@mail.ru>
2021-11-22 02:45:16 +0300

    second link

--- testing_final_project/test_product_page.py
+++ testing_final_project/test_product_page.py
@@ -1,14 +1,16 @@
 from .pages.product_page import ProductPage
-import time
 
-def test_guest_can_add_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+links = [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
+    "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+]
+
+@pytest.mark.parametrize("product", links)
+def test_guest_can_add_product_to_basket(browser, product):
+	link = product
 	page = ProductPage(browser, link)
 	page.open()
-	time.sleep(2)
 	page.add_to_basket()
-	time.sleep(2)
 	page.should_equal_title_and_alert()
-	time.sleep(2)
 	page.should_equal_price_and_price_basket()
-	time.sleep(2)
