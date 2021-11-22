276605da94856cd2460df1c74ed64d9f66c6d654
JigSaW <misho_1998@mail.ru>
2021-11-22 17:13:49 +0300

    fix

--- project/test_items.py
+++ project/test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_button_add_basket(browser):
+def test_add_to_cart_button_is_displayed(browser):
 	link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 	browser.get(link)
 	button = browser.find_element_by_class_name("btn-add-to-basket")
