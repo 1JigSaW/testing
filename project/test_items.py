324af8f3c820afb82ff6f1bca9d6141110d4e0c3
JigSaW <misho_1998@mail.ru>
2021-11-22 17:07:25 +0300

    fix

--- project/test_items.py
+++ project/test_items.py
@@ -1,5 +1,8 @@
+import time
+
 def test_button_add_basket(browser):
 	link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 	browser.get(link)
-	button = browser.find_element_by_class_name("btn-add-to-basket").text
-	assert button == 'Ajouter au panier'
+	button = browser.find_element_by_class_name("btn-add-to-basket")
+	assert button is not None, "There is no add to basket button on the page"
+	time.sleep(30)
