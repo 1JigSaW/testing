14d68c231be2fa4352f12381d44e64a2156ebec1
JigSaW <misho_1998@mail.ru>
2021-11-22 04:10:08 +0300

    test new methods

--- testing_final_project/test_product_page.py
+++ testing_final_project/test_product_page.py
@@ -2,6 +2,8 @@ import pytest
 from .pages.product_page import ProductPage
 import time
 
+one_link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/'
+
 links = [
 	"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 	"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -24,4 +26,22 @@ def test_guest_can_add_product_to_basket(browser, link):
 	page.add_to_basket()
 	page.should_equal_title_and_alert()
 	page.should_equal_price_and_price_basket()
+	page.should_not_disappeared_success_message()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+	page = ProductPage(browser, one_link)
+	page.open()
+	page.add_to_basket()
+	page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+	page = ProductPage(browser, one_link)
+	page.open()
+	page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+	page = ProductPage(browser, one_link)
+	page.open()
+	page.add_to_basket()
 	page.should_not_disappeared_success_message()
